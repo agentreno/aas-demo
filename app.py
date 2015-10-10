e4d44663affc8bc5df24791102f066132492d605
agentreno <kturrell@hotmail.com>
2015-10-10 12:46:34 +0100

    Basic vulnerable bulletin board app

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, redirect, request
+app = Flask(__name__)
+
+posts = []
+
+@app.route("/")
+def index():
+   # Render the main page
+   output = """
+      <form action='sendmsg' method='post'>
+         <input type='text' name='msg'>
+         <input type='submit' value='Submit'>
+      </form>
+      <form action='refresh'>
+         <input type='hidden' name='next' value='/'>
+         <input type='submit' value='Refresh'>
+      </form>
+      <h1>POSTS:</h1>
+   """
+   for post in posts:
+      output += "<p>" + post + "</p>"
+   return output
+
+@app.route("/sendmsg", methods=["POST"])
+def post():
+   # Add message to the list of posts and go back to the main page
+   posts.append(request.form["msg"])
+   return redirect("/")
+
+@app.route("/refresh")
+def refresh():
+   nexturl = request.args.get("next").lower()
+   return redirect(nexturl)
+
+if __name__ == "__main__":
+   app.run(debug=True)
